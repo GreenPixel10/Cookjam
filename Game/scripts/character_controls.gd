763454198b6c70c8456d7565f9450926a3cf1a8a
GreenPixel10 <evan@raz-family.ca>
2025-11-25 20:43:10 -0500

    holding and throwing objects!

--- Game/scripts/character_controls.gd
+++ Game/scripts/character_controls.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
 var move = Vector2(0,0)
-
 var move_speed = 10
-
 var hp = 100
 
 @onready var weapon = $Knife
+var holding = null
+var hands
 
 func _ready():
 	SG.Player = self
+	hands = $hands
 	
 	
 func apply_damage(dam):
@@ -20,9 +21,20 @@ func apply_damage(dam):
 func _input(event: InputEvent):
 	if event.is_action_pressed("attack"):
 		weapon.attack()
+		
+		
+	if event.is_action_pressed("hold"):
+		if(holding != null): #if holding something already, toss it
+			var direction = (get_global_mouse_position() - global_position).normalized()
+			holding.toss(direction)
+			holding = null	
+		else: #if not holding anything, check if anything is there and if so pick one up
+			var within_grab_range = $grab.get_overlapping_areas()
+			if len(within_grab_range) > 0:
+				holding = within_grab_range[0].get_parent()
+				holding.pick_up()
 	
 func _physics_process(delta: float):
-	
 	#read key inut
 	if Input.is_action_pressed("move_left"):
 		move += (Vector2(-move_speed, 0))
--- Game/scripts/pickup.gd
+++ Game/scripts/pickup.gd
@@ -1,16 +1,26 @@
-extends Node2D
+extends RigidBody2D
 
 
 
+
+
+
+enum states {  
+	DROPPED,      
+	HELD,  
+	INUSE  
+} 
 
 var type = "none"
-
-
-
+var state = states.DROPPED
 var t = 0
+var bobbing = false
+
+
+var toss_speed = 10
 
 func _ready() -> void:
-	pass
+	freeze = false
 	
 func init(item_type):
 	type = item_type
@@ -18,6 +28,23 @@ func init(item_type):
 	print("spawning item of type " + type)
 
 func _process(delta: float) -> void:
-	t += delta
-	$Sprite.position.y = sin(t) * 10
 	
+	#make it bob up and down (prob dont need)
+	if bobbing and state == states.DROPPED: 
+		t += delta
+		$Sprite.position.y = sin(t) * 10
+
+#release the item from the player and toss the item
+func toss(direction):
+	reparent(SG.SpawnManager.get_parent()) #add it to the map
+	state = states.DROPPED
+	freeze = false #turn on physics
+	apply_impulse(direction * toss_speed * 10) #push out
+	#you can change the toss speed but it works better to make the pickup object rigid body lighter
+
+#give the item to the player
+func pick_up():
+	reparent(SG.Player.hands) #stick it to the player's hands
+	position = Vector2(0,0)
+	state = states.HELD
+	freeze = true #pause physics
