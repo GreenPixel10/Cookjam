4deffea5c2687a9f41a5d8026ea089802f6ad5a1
GreenPixel10 <evan@raz-family.ca>
2025-11-27 17:52:43 -0500

    finsihed converters w/ sprites

--- Game/scripts/converter.gd
+++ Game/scripts/converter.gd
@@ -9,12 +9,16 @@ var conversion_table
 var item_in = null
 var item_out = null
 
+var sprite = null
+
 func _ready():
 	conversion_table = ObjectManager.conversions[type]
+	sprite = get_parent().get_node_or_null("Sprite")
 	
 
 #signal from collision detector
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	if item_in != null: return #already cookin
 	var item = area.get_parent()
 	var item_name = item.type
 	if conversion_table.has(item_name): #see if this item can be taken
@@ -25,6 +29,9 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		$Timer.start(conversion_time) #start the clock!
 		item.queue_free() #delete the input item
 
+		if sprite != null:
+			sprite.animation = "in_use"
+
 #signal from timer
 func _on_timer_timeout() -> void:
 	var new_object = SG.SpawnManager.spawn_pickup(item_out, global_position)
@@ -34,3 +41,6 @@ func _on_timer_timeout() -> void:
 	print("converted " + item_in + " to " + item_out + "!")
 	item_in = null
 	item_out = null
+	
+	if sprite != null:
+		sprite.animation = "idle"
